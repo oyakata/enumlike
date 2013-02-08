ca4f7a0a37b9efb829ee13d620d61929af461ab3
oyakata <imagawa.hougikumaru@gmail.com>
2013-02-08 11:25:56 +0900

    マニフェストを追加。

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import os
 
 from setuptools import setup
 
+
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
